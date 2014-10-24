81b366cfba20c896f0054ccf64fad02547c1294b
NateRudolph <naterudolph@Nates-MacBook-Pro.local>
2014-10-23 23:15:46 -0400

    add two column layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $( document ).ready(function(){
-    
+    $(".verse p").each(function(){
+        var text = $(this).text().split(' ');
+        var len = text.length;
+        var result = [];
+        
+        for(var i=0; i<len; i++){
+            result[i] = "<span class='word'>" + text[i] + "</span>";
+        }
+        $(this).html(result.join(' '));
+    });
 });
